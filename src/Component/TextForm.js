7ab41a7ce912ad82e0f6a9e09940222fdd9fe255
sumitraj04 <sumit_raj@outlook.com>
2022-07-23 15:10:47 +0530

    Updated href and read time

--- src/Component/TextForm.js
+++ src/Component/TextForm.js
@@ -33,9 +33,10 @@ export default function TextForm(props) {
     <div className='container my-3'>
         <h3>Your text summarry:</h3>
         <p>{
-        document.getElementById('myBox') === null? 0:
-        text.trim() === ''? 0:text.trimStart().split(/[ ]+/).length} words and {text.length} characters are used.</p>
-        <p>It's approx {.008 * text.trimStart().split(/[ ]+/).length} min reads.</p>
+            document.getElementById('myBox') === null? 0:
+            text.trim() === ''? 0:text.trimStart().split(/[ ]+/).length} words and {text.length} characters are used.
+        </p>
+        <p>It's approx {.008 * text.trim().split('').length} min reads.</p>
     </div>
     </>
   )
